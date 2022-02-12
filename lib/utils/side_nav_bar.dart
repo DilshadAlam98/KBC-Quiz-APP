2f0da5e6c02efbecfc7ac65a5a7ec7aa87f29f01
Rudraksh Shukla <rudrakshshukla451@gmail.com>
2022-02-12 14:40:08 +0530

    Fixes- Ontap consistancy of listtile Fixes- navigating to profile screen and leaderboard

--- lib/utils/side_nav_bar.dart
+++ lib/utils/side_nav_bar.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:quiz_app/screen/profile/profile.dart';
 import 'package:quiz_app/service/auth.dart';
 
 class SideNavBar extends StatelessWidget {
@@ -17,33 +18,38 @@ FirebaseAuth auth= FirebaseAuth.instance;
         child: ListView(
           // padding: EdgeInsets.symmetric(horizontal: 20),
           children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "Dilshad",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("RS.50000")
-                    ],
-                  )
-                ],
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Profile(),));
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.white,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Dilshad",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text("RS.50000")
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
             Container(
@@ -56,18 +62,22 @@ FirebaseAuth auth= FirebaseAuth.instance;
             const SizedBox(
               height: 40,
             ),
-            ListItem( "Daily Quiz", Icons.quiz,context),
-            ListItem("LeadBoard",  Icons.leaderboard,context),
-            ListItem( "How to use",  Icons.question_answer,context),
-            ListItem( "About us",  Icons.face,context),
-            ListItem( "Log out", Icons.logout,context)
+            ListItem( label: "Daily Quiz",icon: Icons.quiz,context: context,ontap: (){}),
+            ListItem( label: "LeadBoard",icon: Icons.quiz,context: context,ontap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => Profile(),));
+            }),
+            ListItem( label: "How to use",icon: Icons.quiz,context: context,ontap: (){}),
+            ListItem( label: "About us",icon: Icons.quiz,context: context,ontap: (){}),
+            ListItem( label: "Log out",icon: Icons.quiz,context: context,ontap: (){
+              signOutUser(context);
+            }),
           ],
         ),
       ),
     );
   }
 
-  Widget ListItem( String label,  IconData icon, BuildContext context) {
+  Widget ListItem({required String label, required IconData icon, required VoidCallback ontap, required BuildContext context}) {
     final color = Colors.white;
     final hoverColor = Colors.white60;
     return ListTile(
@@ -80,9 +90,7 @@ FirebaseAuth auth= FirebaseAuth.instance;
         label,
         style: TextStyle(color: color),
       ),
-      onTap: ()async{
-        signOutUser(context);
-      },
+      onTap:ontap
     );
   }
 }
